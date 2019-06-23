10bc2033bc9bdd75bdac407921a6849a19706889
Maxime MORGE <maxime.morge@univ-lille1.fr>
2019-06-23 22:34:18 +0200

    First running version

---
+++ src/main/scala/cristalsmac/mas4data/sample/Hulk.scala
@@ -0,0 +1,104 @@
+//Copyright (C) Quentin BAERT 2018
+package cristalsmac.mas4data.sample
+
+import akka.actor._
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+import scala.io.StdIn
+
+/**
+  * The messages
+  */
+sealed trait HulkMessage
+object Start extends HulkMessage
+object Flick extends HulkMessage
+object Warning extends HulkMessage
+object HulkSmash extends HulkMessage
+object Calm extends HulkMessage
+
+/**
+  * The state of Bruce
+  */
+sealed trait BruceState
+case object Banner extends BruceState
+case object Hulk extends BruceState
+
+/**
+  * The mind of Bruce
+  * @param patience is an integer representation of his memory
+  */
+final case class Mind(patience: Int = 3)
+
+/**
+  * Bruce is an agent, i.e. an actor with a behaviour and a state of mind
+  */
+class Bruce extends Actor with FSM[BruceState, Mind] {
+  // Bruce starts as Banner with patience
+  startWith(Banner, Mind())
+
+  // When Bruce is Banner, flicks decrease his patience until he becomes Hulk
+  when(Banner) {
+    case Event(Flick, mind) if mind.patience > 0 =>
+      println("Bruce: -Hum...")
+      stay using Mind(mind.patience-1)
+    case Event(Flick, mind) if mind.patience == 0 =>
+      println("Bruce: -Grrr...")
+      goto(Hulk) using Mind(0) replying Warning
+  }
+
+  // When Bruce is Hulk, flicks imply a smash and he calms down, i.e. become Banner
+  when(Hulk) {
+    case Event(Flick, _) =>
+      stay replying{
+        sender ! HulkSmash
+        self ! Calm
+      }
+    case Event(Calm, _) =>
+      goto(Banner) using Mind()
+  }
+
+  // Monitoring
+  onTransition {
+    case from -> to=> println(s"Bruce was $from and he becomes $to")
+  }
+
+  // Initialize Bruce
+  initialize()
+}
+
+/**
+  * Reckless agent
+  * @param bruce is his partner
+  */
+class Reckless(bruce: ActorRef) extends Actor {
+  def receive : PartialFunction[Any,Unit] = {
+    case Start =>
+      for(i <- 1 to 5){
+        bruce ! Flick
+      }
+    case Warning =>
+      println("Reckless: What ?!")
+    case HulkSmash =>
+      println("Reckless: Ouch !")
+  }
+}
+
+/**
+  * Test Bruce
+  */
+object TestHulk extends App {
+  import Hulk._
+
+  // Create the actor system
+  val system: ActorSystem = ActorSystem("TestHulk")
+
+  try {
+    // Create Bruce and the reckless agent
+    val bruce: ActorRef = system.actorOf(Props(classOf[Bruce]))
+    val reckless: ActorRef = system.actorOf(Props(classOf[Reckless], bruce))
+    reckless ! Start
+    println(">>> Press ENTER to exit <<<")
+    StdIn.readLine()
+  } finally {
+    system.terminate()
+  }
+}
